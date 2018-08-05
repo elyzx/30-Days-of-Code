65b9e4d92027906c88363363be1b42bfbb03b814
Ellen Children <ellenchildren@icloud.com>
2018-08-05 10:34:52 +0100

    Create Solution.py

---
+++ 5 - Loops/Solution.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input())
+    
+for i in range(1, 11):
+    print('{0} x {1} = {2}'.format(n, i, n * i))
